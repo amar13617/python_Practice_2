f0bfc0a1c583bbcd41030646f0aa11e1c2fd1e35
amar13617 <amar13617@gmail.com>
2023-01-03 21:42:59 +0530

    getting key from max value

--- PyTest2.py
+++ PyTest2.py
@@ -1,1 +1,13 @@
-print("hello")
+mydict = { 'a':304, 'e':53, 'g':1000, 'h':100 , 'f': 200}
+counter = 0
+dict = []
+for i in mydict:
+    if mydict[i] > counter:
+        counter = mydict[i]
+        if counter == mydict[i] :
+            dict.append(i)
+
+#print(dict)
+mydict = { 'a':304, 'e':53, 'g':1000, 'h':100 , 'f': 200}
+maxkey = max(mydict, key = mydict.get)
+print(maxkey)
