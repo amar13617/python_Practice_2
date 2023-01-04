aa6545219569774cf9144a1dfd5f8cafafdd6b3f
amar13617 <amar13617@gmail.com>
2023-01-04 12:10:44 +0530

    Concept of class and object

--- PyTest2.py
+++ PyTest2.py
@@ -10,4 +10,32 @@ for i in mydict:
 #print(dict)
 mydict = { 'a':304, 'e':53, 'g':1000, 'h':100 , 'f': 200}
 maxkey = max(mydict, key = mydict.get)
-print(maxkey)
+#print(maxkey)
+
+# Concept of class
+class Car:
+
+    def __init__(self,modelname,year):
+        self.modelname = modelname
+        self.year = year
+
+    def display(self):
+        print(self.modelname, self.year)
+c1 = Car("Toyota", 2016)
+c1.display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
